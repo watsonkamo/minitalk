e036c42ae7a4626f7836f0aa5eea60fc0832ce4f
watosonkamo <umdroid.1917@gmail.com>
2024-03-31 01:15:36 +0900

    解説用

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: eshintan <eshintan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:42:48 by eshintan          #+#    #+#             */
-/*   Updated: 2024/03/30 23:26:34 by eshintan         ###   ########.fr       */
+/*   Updated: 2024/03/31 01:07:29 by eshintan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,20 @@ void	send_bit(pid_t pid, char c)
 	i = 0;
 	while (i < 8)
 	{
-		if (c & 1)
-		{
-			if (kill(pid, SIGUSR1) == -1)
-				exit(1);
-			//kill(pid, SIGUSR1);
-		}
+		if (c & 1)//ビットAND演算を行い,結果が1の場合
+			kill(pid, SIGUSR1);//結果が1の場合
 		else
-		{
-			if (kill(pid, SIGUSR2) == -1)
-				exit(1);
-			//kill(pid, SIGUSR2);
-		}
-		c = c >> 1;
+			kill(pid, SIGUSR2);//結果が0の場合
+		c >>= 1;//cを1ビット右シフト. 1ビット右にずらすと2で割った値になる.右シフトとは2で割ることと同じ
 		i++;
-		usleep(500);
+		usleep(100);//usleepは引数のマイクロ秒だけプログラムを停止する
 	}
 }
 
-void	handler(int signum, siginfo_t *info, void *context)
+void	handler(int signum, siginfo_t *info, void *context)//siginfo_tはシグナルの情報を格納する構造体.
 {
+	//void *contextはシグナルの情報を格納する構造体.
+	//handler関数はシグナルを受け取った時に呼び出される関数
 	(void)context;
 	(void)info;
 	if (signum == SIGUSR1)
@@ -47,27 +41,26 @@ void	handler(int signum, siginfo_t *info, void *context)
 
 int	main(int argc, char **argv)
 {
-	pid_t				pid;
-	char				*str;
-	struct sigaction	act;
+	struct sigaction	act;//sigaction構造体はシグナルの挙動を変更するための構造体
+	pid_t				pid;//pid_tはプロセスIDを表す型
 
 	if (argc != 3)
 	{
-		ft_putstr_fd("Usage: ./client <pid> <string>\n", 1);
+		ft_putstr_fd("Usage: ./client [server PID] [message]\n", 2);
 		return (1);
 	}
 	pid = ft_atoi(argv[1]);
-	str = argv[2];
-	act.sa_sigaction;
-	act.sa_sigaction = handler;
-	act.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &act, NULL);
-	sigaction(SIGUSR2, &act, NULL);
-	while (*str)
+	if (pid < 100 || pid > 99998)
 	{
-		send_bit(pid, *str);
-		str++;
+		ft_putstr_fd("Invalid PID\n", 2);
+		return (1);
 	}
-	send_bit(pid, 0);
+	act.sa_flags = SA_SIGINFO;//sa_flagsはシグナルの挙動を変更するためのフラグ. SA_SIGINFOはシグナルの情報を受け取るためのフラグ
+	act.sa_sigaction = handler;//sa_sigactionはシグナルを受け取った時に呼び出される関数.sigactionはシグナルの挙動を変更する関数
+	sigaction(SIGUSR1, &act, NULL);//SIGUSR1のシグナルを受け取った時にhandler関数を呼び出す//NULLはsigactionの第3引数で,シグナルの挙動を変更するための構造体のアドレスを指定する
+	sigaction(SIGUSR2, &act, NULL);//SIGUSR2のシグナルを受け取った時にhandler関数を呼び出す
+	while (*argv[2])//argv[2]が終端文字でない間
+		send_bit(pid, *argv[2]++);//send_bit関数を呼び出す
+	send_bit(pid, '\0');//send_bit関数を呼び出す. 終端文字を送信、終端文字を送信することでサーバーにメッセージの終了を通知し、改行を出力する
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: eshintan <eshintan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 01:13:41 by eshintan          #+#    #+#             */
-/*   Updated: 2024/03/08 01:20:18 by eshintan         ###   ########.fr       */
+/*   Updated: 2024/03/31 01:05:17 by eshintan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,39 @@
 
 void	handler(int signum, siginfo_t *info, void *context)
 {
-	static char	c;
-	static int	i;
+	static char	c = 0;//このcはなんのため？　→　受け取った文字を格納するため　どこの文字？　→　送られてきた文字
+	static int	i = 0;//このiはなんのため？　→　受け取った文字のビット数を数えるため なぜ数える？　→　文字を受け取るため
 
-	c = 0;
-	i = 0;
-	(void)context;
-	if (signum == SIGUSR1)
-		c = c | (1 << i);
+	(void)context;//contextは使わないのでvoid型のポインタであることを示す
+	if (signum == SIGUSR1)//受け取ったシグナルがSIGUSR1の場合
+		c |= (1 << i);//cに1をiビット左シフトした値をOR演算で代入
 	i++;
-	if (i == 8)
+	if (i == 8)//iが8の場合
 	{
 		if (c == 0)
 		{
-			write(1, "\n", 1);
-			kill(info->si_pid, SIGUSR1);
+			write(1, "\n", 1);//標準出力に改行を出力
+			kill(info->si_pid, SIGUSR1);//info->si_pidには送信元のプロセスIDが格納されている
 		}
 		else
-			write(1, &c, 1);
-		c = 0;
-		i = 0;
+			write(1, &c, 1);//cを標準出力に出力
+		i = 0;//iを0に戻す 何故？　→　次の文字を受け取るため
+		c = 0;//cを0に戻
 	}
 }
 
 int	main(void)
 {
-	pid_t				pid;
 	struct sigaction	act;
+	pid_t				pid;
 
 	pid = getpid();
-	ft_putstr_fd("server PID:", 1);
-	ft_putnbr_fd(pid, 1);
-	act.sa_flags = SA_SIGINFO;
-	act.sa_sigaction = handler;
-	sigaction(SIGUSR1, &act, NULL);
-	sigaction(SIGUSR2, &act, NULL);
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(pid, 1);//サーバーのPIDを表示
+	act.sa_flags = SA_SIGINFO;//sa_flagsはシグナルの挙動を変更するためのフラグ. SA_SIGINFOはシグナルの情報を受け取るためのフラグ
+	act.sa_sigaction = handler;//sidactionはシグナルの挙動を変更する関数
+	sigaction(SIGUSR1, &act, 0);
+	sigaction(SIGUSR2, &act, 0);
 	while (1)
 		pause();
 	return (0);
